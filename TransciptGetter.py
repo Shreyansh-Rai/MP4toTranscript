f58c1c6053f330c17ce8b14a8ebf15e2da36412d
Genkai <75561611+Shreyansh-Rai@users.noreply.github.com>
2022-06-06 10:40:17 +0530

    Add files via upload

--- TransciptGetter.py
+++ TransciptGetter.py
@@ -1,20 +1,74 @@
 import wave, math, contextlib
-import speech_recognition as sr
+import os
 from moviepy.editor import AudioFileClip
-transcribed_audio_file_name = "transcribed_speech.wav"
-video_file_name = input("Enter the file name: ")
-audioclip = AudioFileClip(video_file_name)
-audioclip.write_audiofile(transcribed_audio_file_name)
-with contextlib.closing(wave.open(transcribed_audio_file_name,'r')) as f:
-    frames = f.getnframes()
-    rate = f.getframerate()
-    duration = frames / float(rate)
-total_duration = math.ceil(duration / 60)
-r = sr.Recognizer()
-for i in range(0, total_duration):
-    with sr.AudioFile(transcribed_audio_file_name) as source:
-        audio = r.record(source, offset=i*60, duration=60)
-    f = open("transcription.txt", "a")
-    f.write(r.recognize_google(audio))
-    f.write(" ")
-f.close()
+# transcribed_audio_file_name = "transcribed_speech.wav"
+# video_file_name = input("Enter the file name: ")
+# audioclip = AudioFileClip(video_file_name)
+
+from pydub import AudioSegment
+import math
+
+class SplitWavAudio():
+    def __init__(self, folder, filename):
+        self.folder = folder
+        self.filename = filename
+        self.filepath = folder + '\\' + filename
+        
+        self.audio = AudioSegment.from_file(self.filepath)
+    
+    def get_duration(self):
+        return self.audio.duration_seconds
+    
+    def single_split(self, from_min, to_min, split_filename):
+        t1 = from_min * 60 * 1000
+        t2 = to_min * 60 * 1000
+        split_audio = self.audio[t1:t2]
+        split_audio.export(self.folder + '\\' + split_filename, format="wav")
+    def numsplit(self):
+        total_mins = math.ceil(self.get_duration() / 60)
+        return total_mins 
+    def multiple_split(self, min_per_split):
+        total_mins = math.ceil(self.get_duration() / 60)
+        print(total_mins)
+        for i in range(0, total_mins, min_per_split):
+            split_fn = str(i) + '_' + self.filename
+            self.single_split(i, i+min_per_split, split_fn)
+            print(str(i) + ' Done')
+            if i == total_mins - min_per_split:
+                print('All splited successfully')
+
+class SplitWavAudioSec():
+    def __init__(self, folder, filename):
+        self.folder = folder
+        self.filename = filename
+        self.filepath = folder + '\\' + filename
+        
+        self.audio = AudioSegment.from_file(self.filepath)
+    
+    def get_duration(self):
+        return self.audio.duration_seconds
+    
+    def single_split(self, from_sec, to_sec, split_filename):
+        t1 = from_sec * 1000
+        t2 = to_sec  * 1000
+        split_audio = self.audio[t1:t2]
+        split_audio.export(self.folder + '\\' + split_filename, format="wav")
+    def numsplit(self,seconds_per_split):
+        total_seconds = math.ceil(self.get_duration())
+        num=0
+        for i in range(0, total_seconds, seconds_per_split):
+            num=num+1
+            if i == total_seconds - seconds_per_split:
+                print('All splited successfully')
+        return num
+    def multiple_split(self, seconds_per_split):
+        total_seconds = math.ceil(self.get_duration())
+        num = 0
+        print(total_seconds)
+        for i in range(0, total_seconds, seconds_per_split):
+            split_fn = str(num) + '_' + self.filename
+            num+=1
+            self.single_split(i, i+seconds_per_split, split_fn)
+            print(str(i) + ' Done')
+            
+        print('All splited successfully')
--- stt_cloud.py
+++ stt_cloud.py
@@ -1,66 +1,95 @@
 import os
+from warnings import catch_warnings
 from google.cloud import speech
 from moviepy.editor import AudioFileClip
+import sys
+from TransciptGetter import SplitWavAudio,SplitWavAudioSec
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'client_service_key.json' #to use the speech to text api.
 #https://www.youtube.com/watch?v=lKra6E_tp5U useful link.
 speech_client = speech.SpeechClient()
 
-transcribed_audio_file_name = "transcribed_speech.wav"
-video_file_name = input("Enter the file name: ") #Takes the MP4 file name as input.
-audioclip = AudioFileClip(video_file_name) 
-audioclip.write_audiofile(transcribed_audio_file_name) #Creates the wav file
-# Transcribe Local Media File 
-# File Size: < 10mbs, length < 1 minute
-# Step 1. Load the media files
+def getTranscript(fname):
+    media_file_name_wav = fname
+    with open(media_file_name_wav, 'rb') as f2:
+        byte_data_wav = f2.read()
+    audio_wav = speech.RecognitionAudio(content=byte_data_wav)
+    config_wav = speech.RecognitionConfig(
+        sample_rate_hertz=44100,
+        enable_automatic_punctuation=True,
+        language_code='en-US',
+        audio_channel_count=2
+    )
+    response_standard_wav = speech_client.recognize(
+        config=config_wav,
+        audio=audio_wav
+    )
 
-media_file_name_wav = 'transcribed_speech.wav' #the wav that we created was called transcribed_speech.wav
+    return response_standard_wav
 
+#Slicing Files and stitiching again.
 
-with open(media_file_name_wav, 'rb') as f2:
-    byte_data_wav = f2.read()
-audio_wav = speech.RecognitionAudio(content=byte_data_wav)
-
-
-config_wav = speech.RecognitionConfig(
-    sample_rate_hertz=44100,
-    enable_automatic_punctuation=True,
-    language_code='en-US',
-    audio_channel_count=2
-)
-
-response_standard_wav = speech_client.recognize(
-    config=config_wav,
-    audio=audio_wav
-)
-
-print(response_standard_wav)
-
+f = open("transcripts.txt",'a')
+count = 1
+for i in range(1000):
+    try:
+        print("Video number ",count)
+        media_path = 'D:\\Internships\\MPL_PR\\VideoFolder\\vid'+str(count)+'.mp3'
+        swa = SplitWavAudioSec('D:\\Internships\\MPL_PR\\VideoFolder','vid'+str(count)+'.mp3')
+        swa.multiple_split(30)
+        numsplits = swa.numsplit(30)
+        print(numsplits)
+        f.write('"')
+        for v in range(numsplits):
+            mpath = 'D:\\Internships\\MPL_PR\\VideoFolder\\'+str(v)+'_vid'+str(count)+'.mp3'
+            print(mpath)
+            resp = getTranscript(mpath)
+            print(resp)
+            for result in resp.results:
+                print(result.alternatives[0].transcript)
+                f.write(result.alternatives[0].transcript)
+                f.write(" ")
+                print(result.alternatives[0].confidence)
+                print()
+        count = count +1 
+        f.write('",')
+    except:
+        print("End of files")
+        break
 
 # Transcribing a long media file
 
-#enter the uri to the larger file after uploading to cloud bucket.
+# enter the uri to the larger file after uploading to cloud bucket.
+# f = open("transcripts.txt", "a")
+# count=1
 
+# for i in range(1000):
+#     try :
+#         media_uri = 'gs://mplstttest/vid'+str(count)+'.mp3' #change project name as per need.
+#         count= count+1
+        # long_audi_wav = speech.RecognitionAudio(uri=media_uri)
+        # config_wav_enhanced = speech.RecognitionConfig(
+        # sample_rate_hertz=48000,
+        # enable_automatic_punctuation=True,
+        # language_code='en-US',
+        # use_enhanced=True,
+        # model='video'
+        # )
 
-# media_uri = ''
-# long_audi_wav = speech.RecognitionAudio(uri=media_uri)
-
-# config_wav_enhanced = speech.RecognitionConfig(
-#     sample_rate_hertz=48000,
-#     enable_automatic_punctuation=True,
-#     language_code='en-US',
-#     use_enhanced=True,
-#     model='video'
-# )
-
-# operation = speech_client.long_running_recognize(
-#     config=config_wav,
-#     audio=long_audi_wav
-# )
-# response = operation.result(timeout=90)
-# print(response)
-
-# for result in response.results:
-#     print(result.alternatives[0].transcript)
-#     print(result.alternatives[0].confidence)
-#     print()
+        # operation = speech_client.long_running_recognize(
+        # config=config_wav,
+        # audio=long_audi_wav
+        # )
+        # response = operation.result(timeout=90)
+#         print(response)
+        # f.write('"')
+        # for result in response.results:
+        #     print(result.alternatives[0].transcript)
+        #     f.write(result.alternatives[0].transcript)
+        #     f.write(" ")
+        #     print(result.alternatives[0].confidence)
+        #     print()
+        # f.write('",')
+#     except:
+#         print("End of files in cloud...")
+#         break
